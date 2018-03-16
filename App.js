e3021a849b74eaa12e484e37e378b51dab47ed27
Rory0172 <rory0172@live.nl>
2018-03-16 12:05:53 +0100

    add searchbar to find location google maps

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 import FetchLocation from './components/FetchLocation';
 import UsersMap from './components/UsersMap';
 
 export default class App extends React.Component {
   state = {
-    userLocation: null
+    userLocation: null,
+    text: ''
   };
 
   getUserLocationHandler = () => {
@@ -26,11 +27,43 @@ export default class App extends React.Component {
     );
   };
 
+  findCity = () => {
+    return fetch(
+      'https://maps.googleapis.com/maps/api/geocode/json?address=' +
+        this.state.text +
+        ',+NL&key=AIzaSyDL63JVLEhpIIrVZzVHUFZZj7f6yi9DG7o'
+    )
+      .then(response => response.json())
+      .then(responseJson => {
+        this.location = responseJson.results[0].geometry.location;
+        this.setState({
+          userLocation: {
+            latitude: this.location.lat,
+            longitude: this.location.lng,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }
+        });
+      });
+  };
+
   render() {
     return (
       <View style={styles.container}>
+        <Button onPress={this.findCity} title="Search" color="#841584" />
         <FetchLocation onGetLocation={this.getUserLocationHandler} />
         <UsersMap userLocation={this.state.userLocation} />
+        <TextInput
+          style={{
+            height: 40,
+            width: '100%',
+            borderColor: 'gray',
+            borderWidth: 1,
+            position: 'absolute'
+          }}
+          onChangeText={text => this.setState({ text })}
+          value={this.state.text}
+        />
       </View>
     );
   }
