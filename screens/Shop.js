4395d2c7df24a5b8d39df5082b1e40de3c873cba
Rory0172 <rory0172@live.nl>
2018-03-27 18:06:02 +0200

    add test info shop

--- screens/Shop.js
+++ screens/Shop.js
@@ -1,23 +1,42 @@
 import React from 'react';
+import { Rating } from 'react-native-elements';
 import {
   Text,
   Button,
   FlatList,
   View,
   TouchableHighlight,
-  Platform
+  Platform,
+  Image
 } from 'react-native';
 import { Container } from '../styles/';
 import { provincies } from '../data';
 
 export default function Shop(props) {
-  console.log(props);
+  let shop = props.navigation.state.params.shop;
+  console.log(shop);
   return (
     <Container>
-      <Text>shop info</Text>
+      <Text>{shop.name}</Text>
+      <Rating
+        type="star"
+        startingValue={shop.stars}
+        imageSize={20}
+        style={{ paddingVertical: 10 }}
+        readonly
+      />
+      <Text>Gedempte Zuiderdiep 63, 9711 HC Amsterdam</Text>
+      <Button title="OPENINGSTIJDEN" onPress={() => console.log('button')} />
+      <Image
+        style={{ width: '100%' }}
+        source={require('../public/coffeeshop-photo.jpg')}
+      />
+      <Container>
+        <Text>beoordeling</Text>
+        <Text>23 beoordelingen</Text>
+      </Container>
     </Container>
   );
 }
-
 // buttons zijn echte native buttons, ze zullen er in ios en android
 // anders uitzien. Als je ze wilt stijlen zul je ze zelf moeten maken.
